dc076320e7a6e44700a28c45145d68b52e67a2df
timggraf <tim.g.graf@gmail.com>
2022-04-23 12:15:43 +0200

    Input chart.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,95 @@ import { ConnectWallet } from '~/components/ConnectWallet'
 import { IncrementCounter } from '~/components/IncrementCounter'
 import { TransactionList } from '~/components/TransactionList'
 import { useCounterContract } from '~/hooks/counter'
+import { Doughnut, Line } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import faker from 'faker'
+
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 const Home: NextPage = () => {
   const { contract: counter } = useCounterContract()
+  const data_dougnut = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
 
+// optional for the charts
+// const options = {
+//   responsive: true,
+//   plugins: {
+//     legend: {
+//       position: 'top' as const,
+//     },
+//     title: {
+//       display: true,
+//       text: 'Chart.js Line Chart',
+//     },
+//   },
+// }
+
+// This is the frontend chart
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  const data_line = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  }
+  
   const { data: counterResult } = useStarknetCall({
     contract: counter,
     method: 'counter',
@@ -33,6 +118,8 @@ const Home: NextPage = () => {
       <IncrementCounter />
       <h2>Recent Transactions</h2>
       <TransactionList />
+      <Doughnut data={data_dougnut} />
+      <Line data = {data_line}/>
     </div>
   )
 }
