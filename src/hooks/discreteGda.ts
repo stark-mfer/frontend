7cdfd6bf02e936283f895d8cae370dd2a3e53504
Rootul Patel <rootulp@gmail.com>
2022-04-23 19:59:17 +0200

    Improve consts

--- src/hooks/discreteGda.ts
+++ src/hooks/discreteGda.ts
@@ -1,11 +1,12 @@
 import { useContract } from '@starknet-react/core'
 import { Abi } from 'starknet'
 
-import ERC721_stark_mfer from '~/abi/ERC721_stark_mfer'
+import ERC721_STARK_MFER_ABI from '~/abi/ERC721_stark_mfer.json'
+import { ERC721_STARK_MFER } from "../constants/contracts"
 
 export function useStarkMfer() {
   return useContract({
-    abi: ERC721_stark_mfer as Abi,
-    address: '0x04ffe192e88fa9239c633c2f6c39ae1b67097d896b8564ae331f9ecb0e9c64fc',
+    abi: ERC721_STARK_MFER_ABI as Abi,
+    address: ERC721_STARK_MFER,
   })
 }
